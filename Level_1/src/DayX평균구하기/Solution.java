b4f5c79de383fd86da8c35a5721272b71c96c0db
harrisbang2 <harrisbang98@gmail.com>
2024-01-10 09:55:08 +0900

    version control

---
+++ Level_1/src/DayX평균구하기/Solution.java
@@ -0,0 +1,11 @@
+package DayX평균구하기;
+
+public class Solution {
+    public double solution(int[] arr) {
+        double answer = 0;
+        for (int i = 0; i < arr.length; i++) {
+            answer += arr[i];
+        }
+        return answer/arr.length;
+    }
+}
