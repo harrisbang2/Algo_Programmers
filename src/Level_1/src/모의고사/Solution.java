a2631e387883255d0ba67f38e22bf2110ea84cb8
harrisbang <harrisbang2@gmail.com>
2024-02-15 20:43:54 +0900

    모의고사

--- src/Level_1/src/모의고사/Solution.java
+++ src/Level_1/src/모의고사/Solution.java
@@ -1,8 +1,34 @@
 package 모의고사;
 
+import java.util.ArrayList;
+import java.util.List;
+
 class Solution {
-    public int[] solution(int[] answers) {
+    public List<Integer> solution(int[] answers) {
+        int [] stu1 = {1,2,3,4,5};
+        int [] stu2={ 2, 1, 2, 3, 2, 4, 2, 5};
+        int [] stu3={ 3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
+        int stu1score =0;
+        int stu2score =0;
+        int stu3score =0;
         int[] answer = {};
-        return answer;
+        //
+        for (int i = 0; i < answers.length; i++) {
+            if(answers[i] == stu1[i%5]) stu1score++ ;
+            if(answers[i] == stu2[i%8]) stu2score++ ;
+            if(answers[i] == stu3[i%10]) stu3score++ ;
+        }
+        int highscore = Math.max(stu1score,Math.max(stu2score,stu3score));
+        List<Integer> ls = new ArrayList<>();
+        if(stu1score == highscore){
+            ls.add(1);
+        }
+        if(stu2score == highscore){
+            ls.add(2);
+        }
+        if(stu3score == highscore){
+            ls.add(3);
+        }
+        return ls;
     }
 }
